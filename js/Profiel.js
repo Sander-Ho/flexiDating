60a9c000aca9d9f73a654611c396d15fa7bce23f
Dennis Neve <Dennis.Neve@ad.core.vdabantwerpen.intra>
2019-09-30 13:08:44 +0200

    profiel pagina met afgeschermde gegevens
    profiel pagina met alle gegevens voor de eigenaar en gedeeltelijke gegevens voor  andere gebruikers

--- js/Profiel.js
+++ js/Profiel.js
@@ -4,26 +4,28 @@ const rooturl = "https://scrumserver.tenobe.org/scrum/api"
 const errorBericht = document.getElementById("errorBericht");
 const profielDiv = document.getElementById("profiel");
 
-let profielId = 16;
+let gebruikerId = 16;   /* id van de gebruiker */
+let profielId = 24;     /* id van het te bezoeken profiel */
+
 
 fetch(rooturl + '/profiel/read_one.php?id=' + profielId)
-.then(function (response) { 
-    if (response.status == 200)
-        response.json().then(ToonProfiel); 
-    else {
+    .then(function (response) { 
+        if (response.status == 200)
+            response.json().then(ToonProfiel); 
+        else {
+            profielDiv.style.display = "";
+            errorBericht.style.display = "block";
+
+            errorBericht.innerText = response.statusText;
+        }
+    })
+    .catch(function (error) {
         profielDiv.style.display = "";
         errorBericht.style.display = "block";
+        errorBericht.innerText = error.name;
 
-        errorBericht.innerText = response.statusText;
-    }
-})
-.catch(function (error) {
-    profielDiv.style.display = "";
-    errorBericht.style.display = "block";
-    errorBericht.innerText = error.name;
-
-    console.log(error);
-});
+        console.log(error);
+    });
 
 
 function ToonProfiel(data) {
@@ -31,9 +33,6 @@ function ToonProfiel(data) {
     profielDiv.style.display = "block";
 
     document.getElementById("nickname").innerText = data.nickname;
-    document.getElementById("naam").innerText = data.voornaam + " " + data.familienaam;
-    document.getElementById("geboortedatum").innerText = data.geboortedatum;
-    document.getElementById("email").innerText = data.email;
     document.getElementById("profielfoto").setAttribute("src", "https://scrumserver.tenobe.org/scrum/img/" + data.foto);
     document.getElementById ("profielfoto").setAttribute("alt", data.nickname);
     document.getElementById("beroep").innerText = data.beroep;
@@ -42,4 +41,10 @@ function ToonProfiel(data) {
     document.getElementById("haarkleur").innerText = data.haarkleur;
     document.getElementById("gewicht").innerText = data.gewicht;
     document.getElementById("grootte").innerText = data.grootte;
+    
+    if (gebruikerId === profielId /* || ontgrendeld? */) {
+        document.getElementById("naam").innerText = data.voornaam + " " + data.familienaam;
+        document.getElementById("geboortedatum").innerText = data.geboortedatum;
+        document.getElementById("email").innerText = data.email;
+    }
 }
