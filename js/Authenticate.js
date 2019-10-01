68e22a37eee81e0f84695baf514524617dfddff8
RaphaelDeBaere <50318624+RaphaelDeBaere@users.noreply.github.com>
2019-10-01 10:35:14 +0200

    Create Authenticate.js

---
+++ js/Authenticate.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+           document.getElementById('submitKnop').addEventListener('click', function (e) {
+               let nickname = document.getElementById('nickname').value;
+               let wachtwoord = document.getElementById('wachtwoord').value;
+
+               let url = 'https://scrumserver.tenobe.org/scrum/api/profiel/authenticate.php';
+               let data = {
+                   nickname: nickname,
+                   wachtwoord: wachtwoord
+               }
+
+               var request = new Request(url, {
+                   method: 'POST',
+                   body: JSON.stringify(data),
+                   headers: new Headers({
+                       'Content-Type': 'application/json'
+                   })
+               });
+
+               fetch(request)
+                   .then(function (resp) { return resp.json(); })
+                   .then(function (data) { console.log(data); })
+                   .catch(function (error) { console.log(error); });
+           });
+       }
+      
