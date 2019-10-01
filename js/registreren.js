5c298434eedb5dccf5b1402cf6060d366eadb8d6
sander horemans <sanderhoremans66@gmail.com>
2019-10-01 10:35:01 +0200

    afwerken registreren

--- js/registreren.js
+++ js/registreren.js
@@ -1,6 +1,7 @@
 "use strict";
-const url = "https://scrumserver.tenobe.org/scrum/api";
-const fotos = ["no_image", "man_1", "man_2", "man_3", "man_4", "man_5", "man_6", "man_7", "man_8", "man_9", "man_10", "man_11", "man_default", "adl", "bram", "danira", "isle_debruyne", "jv", "kurt_decaluwe", "paul_ampersand", "vanessa_vaneeno", "zorro",
+const responseDiv = document.getElementById("response");
+const url = "https://scrumserver.tenobe.org/scrum/api/profiel";
+const fotos = ["no_image", "man_1", "man_2", "man_3", "man_4", "man_5", "man_6", "man_7", "man_8", "man_9", "man_10", "man_11", "man_default", "adl", "bram", "danira", "ilse_debruyne", "jv", "kurt_decaluwe", "paul_ampersand", "vanessa_vaneenoo", "zorro",
 "woman_1", "woman_2", "woman_3", "woman_4", "woman_5", "woman_6", "woman_7", "woman_8", "woman_9", "woman_10", "woman_11", "woman_12", "woman_13", "woman_default"];
 const fotoSelect = document.getElementById("foto");
 for (const foto in fotos) {
@@ -21,8 +22,8 @@ document.getElementById("registreren").onclick = function() {
     const beroep = controleerIngevuld("beroep", 1000);
     const oogkleur = controleerIngevuld("oogkleur", 50);
     const haarkleur = controleerIngevuld("haarkleur", 50);
-    const gewicht = controleerIngevuld("gewicht", 1000);
-    const grootte = controleerIngevuld("grootte", 1000);
+    const gewicht = controleerIngevuldNr("gewicht");
+    const grootte = controleerIngevuldNr("grootte");
     controleerIngevuld("wachtwoord1", 1000);
     const email = document.getElementById("email").value;
     if (!emailregex.test(email) || email.length > 1000) {
@@ -43,6 +44,7 @@ document.getElementById("registreren").onclick = function() {
 
     const foto = document.getElementById("foto").value;
     if (foto === "") {
+        allesok = false;
         document.getElementById("fotoFout").style.display = "block";
     } else {
         document.getElementById("fotoFout").style.display = "none";
@@ -50,6 +52,7 @@ document.getElementById("registreren").onclick = function() {
 
     const geslacht = document.getElementById("sexe").value;
     if (geslacht === "") {
+        allesok = false;
         document.getElementById("sexeFout").style.display = "block";
     } else {
     document.getElementById("sexeFout").style.display = "none";
@@ -57,6 +60,7 @@ document.getElementById("registreren").onclick = function() {
     const geboortedatumWaarde = String(document.getElementById("geboortedatum").value);
     if (geboortedatumWaarde === "" || BerekenLeeftijd(new Date(geboortedatumWaarde)) < 18)
     {
+        allesok = false;
         if (geboortedatumWaarde === "") {
 
         document.getElementById("geboortedatumFout").style.display = "block";
@@ -86,33 +90,32 @@ document.getElementById("registreren").onclick = function() {
         "wachtwoord" : wachtwoord2,
         "lovecoins" : 3
         };
-        fetch(url,
+        fetch(url + "/create.php",
         {
             method: 'POST',
-               headers: new Headers(),
-               mode: 'no-cors',
-               cache: 'default',
             body: JSON.stringify(user)
         }
-        ).then(verwerkresponse);
+        ).then(Verwerkresponse);
 
     }
 };
 
-function verwerkresponse(response) {
-    const responseDiv = document.getElementById("response");
-    responseDiv.text = response;
-        responseDiv.style.display = "block";
-    if (response.ok){
-       responseDiv.style.color = "red";
-    } else {
+function Verwerkresponse(response) {
+    responseDiv.style.display = "block";
+    if (response.status === 201){
        responseDiv.style.color = "black";
+    } else {
+       responseDiv.style.color = "red";
     }
-
-
+    response.json().then(ToonMessage)
 }
-
-document.getElementById("foto").onclick = function () {
+function ToonMessage(data) {
+    if(data.hasOwnProperty('id'))
+        responseDiv.innerText = data.message + "    dit is jouw ID: " + data.id;
+    else
+        responseDiv.innerText = data.message;
+}
+document.getElementById("foto").onchange = function () {
     const fotoString = document.getElementById("foto").value;
     if (fotoString !== "") {
         document.getElementById("fotoVoorbeeld").style.display = "block";
@@ -132,7 +135,16 @@ function BerekenLeeftijd(dateString) {
     }
     return leeftijd;
 }
-
+function controleerIngevuldNr(element) {
+    const waarde = document.getElementById(element).value;
+    if (waarde === "" || waarde.length > 1000 || isNaN(waarde) || waarde < 0 ) {
+        document.getElementById(element + "Fout").style.display = "block";
+        allesok = false;
+    } else {
+        document.getElementById(element + "Fout").style.display = "none";
+        return waarde;
+    }
+}
 function controleerIngevuld(element, validatieLengte) {
     const waarde = document.getElementById(element).value;
     if (waarde === "" || waarde.length > validatieLengte) {
